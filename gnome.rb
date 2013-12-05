16b470936d58143182c469e5bc0e8b218972ca4c
Tyler Logan <logtyl02@evergreen.edu>
2013-12-04 17:11:07 -0800

    It's the lhurgoyf

--- gnome.rb
+++ gnome.rb
@@ -1,1 +1,20 @@
-puts "You are a successful gnome. Congratulations, shorty."
+# zencoding: UTF-8
+
+lhurgoyf = true
+
+if lhurgoyf
+	put "Ach! Hans, run! It's the lhurgoyf!"
+end
+
+hans = unless lhurgoyf
+		true
+	else
+		false
+	end
+
+if hans
+	print "Hans is alive."
+else
+	print "Hans is dead."
+end
+
